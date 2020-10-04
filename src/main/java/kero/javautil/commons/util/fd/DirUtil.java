bbd7d66ac477fe00f5779bf505e4d62ab81bf0b3
kero <kero@DESKTOP-TD25L14.localdomain>
2020-10-04 13:41:20 +0900

    test

--- src/main/java/kero/javautil/commons/util/fd/DirUtil.java
+++ src/main/java/kero/javautil/commons/util/fd/DirUtil.java
@@ -3,7 +3,9 @@ package kero.javautil.commons.util.fd;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
+
 import org.apache.commons.io.FileUtils;
+
 import kero.javautil.commons.exception.AppFileIOException;
 import kero.javautil.commons.exception.AppFileIOException.KIND;
 import kero.javautil.commons.exception.AppFileIOException.TARGET;
@@ -18,6 +20,20 @@ public class DirUtil {
    * @throws AppFileIOException あとで書く
    */
   public static void cleanDirectory(String directory) throws AppFileIOException {
+    try {
+      FileUtils.cleanDirectory(new File(directory));
+    } catch (IOException e) {
+      throw new AppFileIOException(TARGET.DIR, KIND.DELETE_ERROR, Paths.get(directory), e);
+    }
+  }
+
+  /**
+   * 指定したディレクトリを削にする.
+   *
+   * @param directory 削除するディレクトリ
+   * @throws AppFileIOException あとで書く
+   */
+  public static void deleteDirectory(String directory) throws AppFileIOException {
     try {
       FileUtils.cleanDirectory(new File(directory));
     } catch (IOException e) {
